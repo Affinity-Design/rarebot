faac8bfde2aa9ff1b90ac7f1a2cd11422c78172f
Affinity-Design <paul@affinitydesign.ca>
2021-11-13 14:23:45 -0500

    add root health

--- on.js
+++ on.js
@@ -36,19 +36,18 @@ router.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET");
   next();
 });
-router.get("/health", (req, res) => {
-  res.status(200).send("Ok");
-});
 app.use("/api/v1", router);
 const server = http.createServer(app);
 server.listen(3000);
+router.get("/", (req, res) => {
+  res.status(200).send("Ok");
+});
 router.get("/health", (req, res) => {
   const data = {
     uptime: process.uptime(),
     message: "Ok",
     date: new Date(),
   };
-
   res.status(200).send(data);
 });
 
